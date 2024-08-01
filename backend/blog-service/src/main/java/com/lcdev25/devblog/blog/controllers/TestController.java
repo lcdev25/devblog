8d8abd290524efd504f1ee4d667eb79ef65939ed
lcdev25 <lcdev25@gmail.com>
2024-08-01 21:03:13 +0530

    Initialize CI/CD for user service

--- backend/blog-service/src/main/java/com/lcdev25/devblog/blog/controllers/TestController.java
+++ backend/blog-service/src/main/java/com/lcdev25/devblog/blog/controllers/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/v1/test")
 public class TestController {
 
     @GetMapping("")
--- backend/user-service/src/main/java/com/lcdev25/devblog/user/controllers/AuthController.java
+++ backend/user-service/src/main/java/com/lcdev25/devblog/user/controllers/AuthController.java
@@ -1,11 +1,12 @@
 package com.lcdev25.devblog.user.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/v1/auth")
 public class AuthController {
 
     @GetMapping("")
@@ -17,4 +18,10 @@ public class AuthController {
     public String test2() {
         return "Tested and working, let's see again";
     }
+
+    @PostMapping("/redirect")
+    public String redirect() {
+        return "Invoked the redirect method";
+    }
+
 }
